cf88bd3b08e472f97a2df75829ea7a66b08dbf1f
codycmartin <codycmartin89@gmail.com>
2020-05-13 16:09:07 -0700

    user reg constraints in db put in place

--- register.php
+++ register.php
@@ -1,17 +1,18 @@
 <?php
 // register.php
-$pageTitle = "Register";
+$pageTitle = "New User Registration";
 require 'inc/header.inc.php';
 require_once 'inc/db_connect.inc.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $db->real_escape_string($_POST['username']);
     $email = $db->real_escape_string($_POST['email']);
     $first_name = $db->real_escape_string($_POST['first_name']);
     $last_name = $db->real_escape_string($_POST['last_name']);
     $password = hash('sha512', $db->real_escape_string($_POST['password']));
 
-    $sql = "INSERT INTO user (email,first_name,last_name,password) 
-                    VALUES('$email','$first_name','$last_name','$password')";
+    $sql = "INSERT INTO gallery (username,email,first_name,last_name,password) 
+                    VALUES('$username','$email','$first_name','$last_name','$password')";
 
     // echo $sql;
     $result = $db->query($sql);
@@ -27,6 +28,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <h1>Register</h1>
 <form action="register.php" method="POST">
+    <label for="username">Username</label>
+    <input type="text" id="username" required name="username">
+    <br><br>
     <label for="email">Email</label>
     <input type="email" id="email" required name="email">
     <br><br>
